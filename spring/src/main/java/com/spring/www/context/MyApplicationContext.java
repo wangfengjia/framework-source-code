304ef35b131709be8c01724600605107c511392d
wangyongchun <wyc18262608918@163.com>
2021-09-02 21:08:05 +0800

    spring transaction implement code analysis

--- spring/src/main/java/com/spring/www/context/MyApplicationContext.java
+++ spring/src/main/java/com/spring/www/context/MyApplicationContext.java
@@ -1,6 +1,6 @@
 package com.spring.www.context;
 
-import com.spring.www.service.IUserService;
+import com.spring.www.dao.UserMapper;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,13 +9,13 @@ public class MyApplicationContext {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring-transaction-v1.xml"});
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring-transaction-v2.xml"});
 //
 //        User user1 = context.getBean("user1", User.class);
 //        System.out.println(user1.getName());
 //
-        IUserService userService = context.getBean("userService", IUserService.class);
-        userService.create(null);
+        UserMapper userMapper = context.getBean("userDaoTrans", UserMapper.class);
+        userMapper.getById(1);
         System.out.println("test aop");
 
 //        UserMapper userDao = context.getBean("userDaoTrans", UserMapper.class);
